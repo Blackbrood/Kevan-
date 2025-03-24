4a5d75595a524920ecfe72f4bad31f37e8bccce9
Blackbrood <96950354+Blackbrood@users.noreply.github.com>
2025-03-24 15:52:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { spawnSync } = require('child_process')
 const { existsSync, writeFileSync } = require('fs')
 
-const SESSION_ID = 'levanter_2414b9be695a9d4fff81a50496211dfb8b' // youll paste it here and replace this this line only, don't remove ' <- this symbol
+const SESSION_ID = 'levanter_24fe51caf388e64902a19c8b39f62c5e07' // youll paste it here and replace this this line only, don't remove ' <- this symbol
 
 if (!existsSync('levanter')) {
   console.log('Cloning the repository...')
